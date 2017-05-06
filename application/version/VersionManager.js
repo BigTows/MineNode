b3f442eba93227b1ba2daca20aa4269291acf784
bigtows <gasfull98@gmail.com>
2017-05-06 22:52:50 +0300

    Fix mkdir Error callBack

--- application/version/VersionManager.js
+++ application/version/VersionManager.js
@@ -30,8 +30,9 @@ var VersionManager = {
      * @TODO add Custom server
      */
     loadFiles: function load(name) {
-        var path = "/Applications/NodeMine/versions/1.10/";
-        var version = JSON.parse(fs.readFileSync(path + "1.10.json"));
+        var path = "/Applications/NodeMine/versions/1.11.2/";
+        var commandLauncher="java -Djava.library.path="+path+"natives -cp '";
+        var version = JSON.parse(fs.readFileSync(path + "1.11.2.json"));
         var libs = version.libraries;
         var links = [];
         for (var key in libs) {
@@ -45,28 +46,37 @@ var VersionManager = {
         }
         links.forEach(function (fileName) {
             const pathToFiles = new URL(fileName.url);
-            try {
                 var path = "";
                 var arrayDir = pathsda.dirname(pathToFiles.pathname).split(pathsda.sep);
-                //console.log(ss);
                 for (var key in arrayDir) {
                     if (arrayDir[key] != "") {
                         path = path + arrayDir[key] + "/";
-                        fs.mkdirSync("/Applications/NodeMine/libraries/" + path);
+                        console.log(path);
+                        if (!fs.existsSync("/Applications/NodeMine/libraries/" + path)){
+                            fs.mkdirSync("/Applications/NodeMine/libraries/" + path);
+                        }
                     }
                 }
-                var file = fs.createWriteStream("/Applications/NodeMine/libraries" + pathToFiles.pathname);
+            var ttta = "/Applications/NodeMine/libraries" + pathToFiles.pathname;
+                var file = fs.createWriteStream(ttta);
                 https.get(fileName.url, function (response) {
                     response.pipe(file);
                 });
-                console.log("Download: " + pathToFiles + " complete ")
-            } catch (err) {
+            commandLauncher+=ttta+"';";
+                //console.log("Download: " + pathToFiles + " complete ")
 
-            }
         });
+
+        var file = fs.createWriteStream("/Applications/NodeMine/versions/1.11.2/1.11.2.jar");
+        https.get(version.downloads.client.url, function (response) {
+            response.pipe(file);
+        });
+        console.log(commandLauncher);
+
     }
 
 };
+
 
 
 module.exports = VersionManager;
